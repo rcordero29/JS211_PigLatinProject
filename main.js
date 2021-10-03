e51bb7e1e3264f1613ddea1e99eedb0290825a68
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-10-03 15:03:20 -0500

    working project

--- main.js
+++ main.js
@@ -1,73 +1,76 @@
-'use strict';
+// 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-const { threadId } = require('worker_threads');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// // brings in the assert module for unit testing
+// const assert = require('assert');
+// // brings in the readline module to access the command line
+// const readline = require('readline');
+// const { threadId } = require('worker_threads');
+// // use the readline module to print out to the command line
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+
+const textInput = document.getElementById("textInput");
+const textOutput = document.getElementById("textOutput");
+const vowels = ["a", "i", "o", "u", "e"];
+const ending = "ay";
+textInput.addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  const output = input
+    .split(" ")
+    .map((word) => {
+      if (vowels.includes(word[0])) {
+        return word + ending;
+      }
+      let consonantsPrefix = "";
+      for (let i = 0; i < word.length; i++) {
+        const char = word[i];
+        if (vowels.includes(char)) {
+          break;
+        }
+        consonantsPrefix += char;
+      }
+      return (
+        word.substring(consonantsPrefix.length) + consonantsPrefix + ending
+      );
+    })
+    .join(" ");
+  textOutput.innerText = output;
 });
 
-function pigLatin(str) {
-  var PostitonVowel = firstVowel(str);
-  if (PostitonVowel > 0) {
-    return (
-      str.slice(PostitonVowel).trim() +
-      str.slice(0, PostitonVowel).trim() +
-      'ay'.toLowerCase().trim()
-    )
-      .toLowerCase()
-      .trim();
-  }
-  return str + 'yay'.toLowerCase().trim();
-}
-
-function firstVowel(str) {
-  for (let i = 0; i < str.length; i++) {
-    if ('aeiou'.indexOf(str[i]) !== -1) {
-      return i;
-    }
-  }
-}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word', (answer) => {
-    console.log(pigLatin(answer));
-    getPrompt();
-  });
-};
+
 
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
-if (typeof describe === 'function') {
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
-  getPrompt();
-}
+// if (typeof describe === 'function') {
+//   describe('#pigLatin()', () => {
+//     it('should translate a simple word', () => {
+//       assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggyay');
+//       assert.equal(pigLatin('emission'), 'emissionyay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
+//   getPrompt();
+// }
 
 // **********
 //   HINTS
